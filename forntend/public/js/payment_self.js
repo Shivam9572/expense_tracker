2466ca782b1842c7cb30e3815c4a63aea5660f50
Shivam9572 <shivamroy9572@gmail.com>
2025-12-06 15:45:54 +0530

    Update payment_self.js to use dynamic domains

--- forntend/public/js/payment_self.js
+++ forntend/public/js/payment_self.js
@@ -1,9 +1,10 @@
  const cashfree = Cashfree({
             mode: "sandbox",
         });
-
+        const b_domain="http://13.232.126.113:4000";
+         const f_domain="http://13.232.126.113:3000";
         document.getElementById("renderBtn").addEventListener("click", async () => {
-            const url = 'http://localhost:4000/payment/order';
+            const url = '${b_domain}/payment/order';
 
 
             try {
